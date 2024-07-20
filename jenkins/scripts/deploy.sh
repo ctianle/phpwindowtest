2c75d8f3d553ffeafabcf79d6336ad03cf50a0b5
Tian <chuatianle@gmail.com>
2024-07-20 15:23:18 +0800

    update for windows

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /Users/tianle/Desktop/SSD/Lab/permanent-docker/home/lab7b/php-selenium-test/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/chuat/Desktop/phpwindowtest/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
